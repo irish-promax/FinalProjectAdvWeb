3eb210ff776f25b45cb71b67f0bb70f72a799543
Iman Irish Daniel Shaiful Anuar <imanirishdaniel@Imans-MacBook-Air.local>
2022-12-10 01:40:16 +0800

    Edit - Auth, Adminpage, differentiate user, CSS-bootstrap

---
+++ app/Http/Controllers/homeControl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class homeControl extends Controller
+{
+    function redirectFunct(){
+        $typeuser = Auth::user()->usertype;
+        
+        if($typeuser == '1'){
+            return view('admin.adminpage');
+        }
+        else{
+            return view('home');
+        }
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,114 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        @include('CSS-related.header')
-    </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <title>FYP-MS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+        <div id="js-preloader" class="js-preloader">
+            <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            </div>
+        </div>
+    <!-- ***** Preloader End ***** -->
+
+    <!-- ***** Header Area Start ***** -->
+        <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+            <div class="container">
+            <div class="row">
+                <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                                <li>
+                                    <x-app-layout>
+                                    </x-app-layout>
+                                </li>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>&nbsp &nbsp &nbsp 
+                            @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                             </div>
+                        @endif
+                    </ul>        
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+                </div>
+            </div>
+            </div>
+        </header>
+    <!-- ***** Header Area End ***** -->
+
+    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-6 align-self-center">
+                <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
+                    <div class="row">
+                        <div class="col-lg-10">
+                            <h2 style="color:white;">Final Year Management System</h2>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-lg-6">
+                <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                    <img src="assets/images/slider-dec.png" alt="">
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
     
-    <body>
-        @include('CSS-related.navbar')
-    </body>
+
+
+
+
+    <!-- Scripts -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/animation.js"></script>
+    <script src="assets/js/imagesloaded.js"></script>
+    <script src="assets/js/popup.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
 </html>
